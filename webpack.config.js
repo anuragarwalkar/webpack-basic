0e2df99eb0076e88ebb7598630f86254395c55df
Anurag <aarwalka@cisco.com>
2020-09-20 11:49:15 +0530

    added webpack dev server

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+// Placeholders
+/**
+ * [hash]
+ * [chunkhash]
+ * [name]
+ * [id]
+ * [query]
+ * [contenthash]
+ */
+
+module.exports = {
+    mode: 'development',
+    entry: { first: './src/app.js'},
+    output: { 
+        path: path.resolve(__dirname, 'build'),
+        filename: 'js/build.js', // js/1.js js/2.js js/3.js js/4.js
+        publicPath: '/assests/', // server.com/assests/js/1.js
+        libraryTarget: 'umd', // commonjs amd js
+        library: 'anuragLib'
+    },
+    devServer: {
+        port: 3000,
+        contentBase: path.join(__dirname, 'dist')
+    } 
+}
